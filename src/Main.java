9a16d2fbeec100284be66090dcefca468b93175c
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-08-01 21:08:31 +0300

    isPerfectNUmber done

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,18 @@ public class Main {
         System.out.println(isPalindrome(1221));
         System.out.println(isPalindrome(707));
         System.out.println(isPalindrome(11212));
+
+        System.out.println("isPerfectNumber------------------");
+        System.out.println( isPerfectNUmber(6));
+        System.out.println( isPerfectNUmber(28));
+        System.out.println( isPerfectNUmber(5));
+        System.out.println( isPerfectNUmber(-1));
     }
 
-    public static boolean isPalindrome(int number){
+    public static boolean isPalindrome(int number) {
 
-        if(number<0) {int positive = number * -1;
+        if (number < 0) {
+            int positive = number * -1;
             String strNumber = Integer.toString(positive);
 
             //char[] digits = strNumber.toCharArray();
@@ -23,11 +30,10 @@ public class Main {
 
             int numericNumber = Integer.valueOf(strReversed);
 
-            if(positive==numericNumber) return true;
+            if (positive == numericNumber) return true;
 
-            else return false;}
-
-        else {
+            else return false;
+        } else {
             String strNumber = Integer.toString(number);
 
             //char[] digits = strNumber.toCharArray(); chara convert ile de yapılabilir
@@ -41,5 +47,16 @@ public class Main {
 
             else return false;
         }
+    }
+
+    public static boolean isPerfectNUmber(int number) {
+        if (number < 0) return false;
+        int total = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+        return total == number;
     }
 }
